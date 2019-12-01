e77f507a41ccb8568af547e5cf105c9c6428e785
Darin <drnellman@gmail.com>
2019-12-01 13:05:05 +0200

    first commit

---
+++ AppTwo/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=264,unique=True)
+
+    def __str__(self):
+        return str(self.first_name + " " + self.last_name)
